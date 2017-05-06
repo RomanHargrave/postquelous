08ce3a10d048069b4abc66f6a287056a32c99db2
Roman Hargrave <roman@hargrave.info>
2017-05-06 14:18:15 -0500

    reformat UUIDColumnType

--- src/main/scala/info/hargrave/postquel/UUIDColumnType.scala
+++ src/main/scala/info/hargrave/postquel/UUIDColumnType.scala
@@ -8,33 +8,30 @@ import net.noerd.prequel._
   * UUID Column Type Extractor
   */
 class UUIDColumnType(val row: ResultSetRow)
-   extends ColumnType[UUID]
-{
-   override def nextValueOption: Option[ UUID ] =
-      Option(row.nextObject).map(_.asInstanceOf[UUID])
+  extends ColumnType[UUID] {
+  override def nextValueOption: Option[UUID] =
+    Option(row.nextObject).map(_.asInstanceOf[UUID])
 
-   override def columnValueOption(columnName: String): Option[ UUID ] =
-      row.columnObject(columnName).map(_.asInstanceOf[UUID])
+  override def columnValueOption(columnName: String): Option[UUID] =
+    row.columnObject(columnName).map(_.asInstanceOf[UUID])
 }
 
 object UUIDColumnType
-   extends ColumnTypeFactory[UUID]
-{
-   def apply(row: ResultSetRow) = new UUIDColumnType(row)
+  extends ColumnTypeFactory[UUID] {
+  def apply(row: ResultSetRow) = new UUIDColumnType(row)
 }
 
 class UUIDFormattable(override val value: UUID)
-   extends Formattable
-{
-   override def escaped(formatter: SQLFormatter): String =
-      s"${formatter.toSQLString(value.toString)}::uuid"
+  extends Formattable {
+  override def escaped(formatter: SQLFormatter): String =
+    s"${formatter.toSQLString(value.toString)}::uuid"
 
-   override def addTo(stmt: ReusableStatement): Unit =
-      stmt.addObject(value)
+  override def addTo(stmt: ReusableStatement): Unit =
+    stmt.addObject(value)
 }
 
-trait UUIDTypeImplicits
-{
-   implicit def uuid2fmt(uuid: UUID): Formattable = new UUIDFormattable(uuid)
-   implicit def row2uuid(row: ResultSetRow): UUID = UUIDColumnType(row).nextValue
+trait UUIDTypeImplicits {
+  implicit def uuid2fmt(uuid: UUID): Formattable = new UUIDFormattable(uuid)
+
+  implicit def row2uuid(row: ResultSetRow): UUID = UUIDColumnType(row).nextValue
 }
